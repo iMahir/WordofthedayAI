29e43c65c6470a8dcca917e67d8826f96d68f96f
Mahir <128459141+iMahir@users.noreply.github.com>
2024-12-09 22:54:26 +0530

    Delete src/providers/image/playground directory

--- src/providers/image/playground/playground.ts
+++
@@ -1,7 +0,0 @@
-import { PythonShell } from 'python-shell';
-
-
-PythonShell.run('src/providers/image/playground/playground.py', { pythonPath: "python3", args: ['-prompt', 'What happened in World War 2?'] }).then(messages => {
-    console.log('finished');
-    console.log(messages);
-});

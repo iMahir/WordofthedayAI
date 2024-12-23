3d37c9d107623deb7363dccd6c541af0292a2876
Mahir <128459141+iMahir@users.noreply.github.com>
2024-12-23 14:20:43 +0530

    feat(fix): hotfix

--- src/post.ts
+++ src/post.ts
@@ -3,7 +3,7 @@ import { postTweet } from "./client";
 async function post() {
 
     const tweet = await postTweet();
-    if (!tweet.status) throw new Error("Error while posting tweet");
+    console.log(tweet.status);
 
     return true;
 }
